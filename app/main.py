1671057785fb7cead1cff1c536a5e346c6f23be8
Sayananodiam <sayan@anodiam.com>
2026-01-29 18:12:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 )
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(user_router, prefix="/users", tags=["user"])
 
 @app.get("/health")
 def health():
